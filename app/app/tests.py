003cef8376ab739d7a5d018eaa4056ca99ae02ee
Sayan-101198 <sayansinha983@gmail.com>
2025-05-02 19:00:35 +0530

    Last attempt:Fixed Linting Error in tests

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,17 +1,18 @@
 """Test the modules"""
-
 from django.test import SimpleTestCase
+
 from app import calc
 
 
 class Calctest(SimpleTestCase):
     """Test the calc module"""
+
     def test_calc_add(self):
         """Test the add function"""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
     def test_calc_subtract(self):
         """test the substract function"""
         res = calc.substract(10, 15)
         self.assertEqual(res, 5)
-
